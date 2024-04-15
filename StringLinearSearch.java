8663fa3860cbc04bfd75e972dee0856f7e0f2a21
jignesh@gmail.com <105378421+jignesh43@users.noreply.github.com>
2024-04-15 21:22:32 +0530

    LinearSearchinstring

--- StringLinearSearch.java
+++ StringLinearSearch.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class StringLinearSearch {
+import java.lang.annotation.Target;
+import java.sql.Array;
+import java.util.Arrays;
+
+public class StringLinearSearch {
+    public static void main(String[] args) {
+        String name = "Hacker";
+        char Target ='i';
+        boolean v = CharSearchInString(name,Target);
+        System.out.println(v);
+        System.out.println(Arrays.toString(name.toCharArray()));
+
+    }
+    static boolean CharSearchInString(String name, char Target){
+        if (name.length()==0){
+            return false;
+        }
+       /* for (int i =0 ; i<name.length(); i++){
+            if (name.charAt(i)==Target){
+                return true;
+            }*/
+        for (char a : name.toCharArray()) {
+            // Compare each character with the target character
+            if (a == Target) {
+                return true;
+            }
+        }
+
+        return false;
+
+
+    }
 }
